d4ea346d752841b11ccbb335cd52c2cacfd2e761
saurabh <48880982+saurabh3065@users.noreply.github.com>
2020-03-05 16:33:30 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -1,14 +1,61 @@
-const express=require('express');
-const router=express.Router();
 
-const controller=require("../controller/user");
+const express=require("express");
+const mongoose=require("mongoose");
+const user=require("../models/user");
+const bcrypt=require("bcryptjs");
 
+module.exports.user_details= (req,res)=>{
+    console.log("hi there");
+    user.find((err,docs)=>{
+        if(!err){
+            console.log("get api");
+            res.send(docs)}
+        else{console.log("error in retriving data")}
+    });
+};
+exports.user_create = (req,res)=>{
+    console.log("enter in create post")
+   
+    res.json({ msg: "new user created",});
+    console.log(req.body);
 
-router.get("/",controller.user_details);
-router.post("/create",controller.user_create);
-router.put("/:id",controller.user_update);
-router.delete("/:id",controller.user_delete);
+    let users = new user({
+        Name:req.body.Name,
+        Email:req.body.Email,
+        Phone:req.body.Phone,
+        password:req.body.password,
+        Device_token:req.body.Device_token,
+        Access_token:req.body.Access_token,
+        Is_verify:req.body.Is_verify,
+        Location:req.body.Location
 
+    });
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(users.password,salt,(err,hash) => {
+            users.password = hash;
+            users.save()
+            .then(() => console.log('Successsss'))
+            .catch((err) => {
+                console.log('ERRorr- ', err);
+        })
+    })
 
+    console.log(req.body);
+   
+    });
+};
+module.exports.user_update=(req,res)=>{
+    user.findByIdAndUpdate(req.params.id, { $set: req.body }, (err,user) => {
+        if (err) return(err);
+        res.send('User is  udpated.');
+    });
+}
 
-module.exports=router;
+module.exports.user_delete = (req, res)=> {
+    user.findByIdAndRemove(req.params.id,  (err) => {
+         if (err) {console.log(req.body); }
+
+         res.send('Deleted successfully!');
+    })
+};
+
