784f06042490d9e286f20c5ec1261954ec86dcfa
saurabh <48880982+saurabh3065@users.noreply.github.com>
2020-03-05 13:09:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+require("./database/db");
+const express=require("express");
+const bodyparse=require("body-parser");
+const route=require("./routes/user");
+
+const app=express();
+app.use(bodyparse.json());
+app.use(bodyparse.urlencoded({extended:true}));
+
+app.use("/user",route);
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000");
+})
